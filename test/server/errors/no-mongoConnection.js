8a711fe474ba549fe709a03cb53a1856318aa818
Xavier Via <fernando.via@gmail.com>
2013-06-27 14:18:34 -0300

    Nicer error message if you forget mongoConnectoin

---
+++ test/server/errors/no-mongoConnection.js
@@ -0,0 +1,22 @@
+var monje = require('../../../index.js');
+var jstp = require('jstp');
+
+monje.bind(jstp);
+
+var db = new monje.mongodb.Db('test', new monje.mongodb.Server("localhost", 27017));
+db.open( function (err, db) {
+//  monje.mongoConnection = db;
+  
+  jstp.listen({
+    tcp: 33333
+  });
+
+});
+
+// This should raise an error!
+jstp.post({
+  resource: ['User'],
+  body: {
+    "name": "Woot?"
+  }
+});
